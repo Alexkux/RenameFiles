416d6d38ff8275beecbc440704086507ed2f4968
Alexkux <kuznetsov-aa@ya.ru>
2021-11-18 16:16:05 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+
+DIRECTORY = r"C:\TMP"
+
+def rename_files(find_directory):
+    for root, dirs, files in os.walk(find_directory):
+#        for name in dirs:
+#            rename_file(root, name)
+        for name in files:
+            rename_file(root, name)
+
+
+
+def rename_file(root, name):
+    valid_name = get_valid_name(name)
+    old_file = os.path.join(root, name)
+    new_file = os.path.join(root, valid_name)
+    os.rename(old_file, new_file)
+
+
+def get_valid_name(name):
+    name = name.replace("[sharewood.biz] ", "")
+#    if not name.startswith("a_"):
+#        name = "a_" + name
+    return name
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    rename_files(DIRECTORY)
+
+

311197eef0401f6776593e702d1e3c559dfc4fa3
Aleksei Kuznetsov <43678488+Alexkux@users.noreply.github.com>
2021-12-10 18:27:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def rename_files(find_directory):
 
 def rename_file(root, name):
     valid_name = get_valid_name(name)
-    old_file = os.path.join(root, name)
-    new_file = os.path.join(root, valid_name)
+    # old_file = os.path.join(root, name) # Переименование директорий
+    new_file = os.path.join(root, valid_name) # Переименование файлов
     os.rename(old_file, new_file)
 
 
 def get_valid_name(name):
-    name = name.replace("[sharewood.biz] ", "")
+    name = name.replace("Иванов", "")
 #    if not name.startswith("a_"):
 #        name = "a_" + name
     return name
